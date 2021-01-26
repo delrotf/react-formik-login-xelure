4f86d108a8f1d9a4c303f309054ef7a86b7b1bae
delrotf <delrotf02@gmail.com>
2021-01-26 03:46:23 -0500

    4 files modified

--- components/FormCard/FormCard.tsx
+++ components/FormCard/FormCard.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { LoginForm } from "..";
 
+
 const FormCard = props => {
   return (
     <div className="form-card border">
-      <div className="header text-center p-2 text-light">Get started today!</div>
+      <div className="header text-center p-3 text-light">Get started today!</div>
       <div>
         <LoginForm />
       </div>
--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -7,6 +7,8 @@ import {
   LoginContext,
   LoginContextProvider
 } from "../../context/login-context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
 const LoginForm = props => {
   const { setFirstname, setAuthenticated } = useContext(LoginContext);
@@ -19,14 +21,19 @@ const LoginForm = props => {
   };
 
   const validationSchema = Yup.object({
-    firstname: Yup.string().required("This field is required."),
-    lastname: Yup.string().required("This field is required."),
+    firstname: Yup.string()
+      .required("This field is required.")
+      .matches(/^[A-Za-z]+$/, "Numbers are not allowed."),
+    lastname: Yup.string()
+      .required("This field is required.")
+      .matches(/^[A-Za-z]+$/, "Numbers are not allowed."),
     email: Yup.string()
       .required("This field is required.")
-      .email("Invalid format."),
+      .email("Invalid email format."),
     password: Yup.string()
       .required("This field is required.")
       .min(8, "At least ${min} characters.")
+      .matches(/^[0-9A-Za-z]+$/, "Only letters and numbers.")
   });
 
   const onSubmit = values => {
@@ -48,17 +55,13 @@ const LoginForm = props => {
                   controlId="firstname"
                   name="firstname"
                   type="text"
-                  label="Firstname"
-                  hint="firstname"
-                  placeholder="Firstname"
+                  label="First name"
                 />
                 <Input
                   controlId="lastname"
                   name="lastname"
                   type="text"
-                  label="Lastname"
-                  hint="Lastname"
-                  placeholder="Lastname"
+                  label="Last name"
                 />
               </BsForm.Row>
               <BsForm.Row>
@@ -66,25 +69,28 @@ const LoginForm = props => {
                   controlId="email"
                   name="email"
                   type="email"
-                  label="Email"
-                  hint="email"
-                  placeholder="Email"
+                  label="Email address"
                 />
                 <Input
                   controlId="password"
                   name="password"
                   type="password"
                   label="Password"
-                  hint="password"
-                  placeholder="Password"
                 />
               </BsForm.Row>
-            </div>
-            <hr />
-            <div className="d-flex p-3">
-              <Button variant="primary" className="flex-fill" type="submit">
-                Submit
-              </Button>
+              <hr />
+              <div className="d-flex mb-3">
+                <Button
+                  variant="outline-light"
+                  className="flex-fill p-3"
+                  type="submit"
+                >
+                  <span className="mr-2">Claim you free trial</span>
+                  <FontAwesomeIcon icon={faCaretRight} />
+                </Button>
+              </div>
+              <span>You are agreeing to our </span>
+              <a href="#">Terms and Services</a>
             </div>
           </Form>
         )}
