2db058db3c2a6afc9496cdb180651997d82f4197
delrotf <delrotf02@gmail.com>
2021-01-26 03:06:38 -0500

    2 files modified

--- components/FormCard/FormCard.tsx
+++ components/FormCard/FormCard.tsx
@@ -4,7 +4,7 @@ import { LoginForm } from "..";
 const FormCard = props => {
   return (
     <div className="form-card border">
-      <div className="header text-center p-2 text-light">header</div>
+      <div className="header text-center p-2 text-light">Get started today!</div>
       <div>
         <LoginForm />
       </div>
